d39e7fc803a7c3d573a41cb1d59719c7557db27c
gabriel2302 <gabriel23002@gmail.com>
2023-09-10 00:45:15 -0300

    Botao desabilitado

--- components/Button/Button.stories.ts
+++ components/Button/Button.stories.ts
@@ -2,9 +2,19 @@ import { Meta, StoryObj } from "@storybook/react"
 import Button, { ButtonProps } from "./Button"
 
 const meta: Meta<ButtonProps> = {
-  title: 'About/Button',
+  title: 'Molecules/Button',
   component: Button,
-  argTypes: {}
+  argTypes: {
+    children: {
+      type: 'string'
+    },
+    disabled: {
+      type: 'boolean'
+    },
+    className: {
+      type: 'string'
+    }
+  }
 }
 
 export default meta
