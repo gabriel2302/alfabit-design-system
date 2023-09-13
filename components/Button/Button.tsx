381cb18b340a24fa385e5492058335ad1d1808eb
gabriel2302 <gabriel23002@gmail.com>
2023-09-12 21:39:32 -0300

    secondary button

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,22 +1,34 @@
 import React from "react"
 
 export type ButtonProps = {
-  children: React.ReactNode
+  children: React.ReactNode;
+  variant?: 'primary' | 'secondary' | 'tertiary';
 } & React.ButtonHTMLAttributes<HTMLButtonElement>
 
-const Button = ({ children, className, disabled, ...rest }: ButtonProps) => {
-  const generalStyle = 'rounded-md px-6 py-2'
-  const Btn = (classes: string) => {
-    return (
-      <button
-      className={`${generalStyle} ${classes} ${className || ''}
+
+function getVariant(variant: ButtonProps['variant'], disabled: ButtonProps['disabled']) {
+  switch (variant) {
+    case 'primary':
+      return disabled ? 'bg-disabled text-disabled' : 'bg-primary text-white'
+    case 'secondary':
+      return disabled ? 'bg-disabled text-disabled' : 'bg-quaternary text-primary'
+    case 'tertiary':
+      return disabled ? '' : ''
+    default:
+      return disabled ? '' : ''
+  }
+}
+
+const Button = ({ children, variant = 'primary', className, disabled, ...rest }: ButtonProps) => {
+  return (
+    <button
+      className={`rounded-md px-6 py-2 ${getVariant(variant, disabled)} ${className || ''}
       `}
       disabled={disabled}
       {...rest}
-    >{children}</button>
-    )
-  }
-  return Btn(disabled ? 'bg-disabled text-disabled' : 'bg-primary text-white')
+    >{children}
+    </button>
+  )
 }
 
 export default Button
