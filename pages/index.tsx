c18e2152147ea1d5a284401280421c3101ec65d4
gabriel2302 <gabriel23002@gmail.com>
2023-09-17 22:14:11 -0300

    Switch Component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
 import Box from "@/components/Box/Box"
 import Button from "@/components/Button/Button"
+import Switch from "@/components/Switch/Switch"
 
 const App = () => {
   return (
     <Box type="alert">
-      <Button>Botao</Button>
+      <Switch variant="contract" />
     </Box>
   )
 }
