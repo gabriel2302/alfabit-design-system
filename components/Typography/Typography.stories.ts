6de9e1e9ff192309351efa8df09b767799db523d
gabriel2302 <gabriel23002@gmail.com>
2023-09-19 07:00:38 -0300

    Colors Palette

--- components/Typography/Typography.stories.ts
+++ components/Typography/Typography.stories.ts
@@ -19,3 +19,39 @@ export const Primary: StoryObj<TypographyProps> = {
     children: 'Text'
   }
 }
+
+export const TextHeadingH1: StoryObj<TypographyProps> = {
+  args: {
+    children: 'Text',
+    element: 'h1',
+    size: 'title1',
+    className: 'font-extrabold'
+  }
+}
+
+export const TextHeadingH2: StoryObj<TypographyProps> = {
+  args: {
+    children: 'Text',
+    element: 'h2',
+    size: 'title2',
+    className: 'font-extrabold'
+  }
+}
+
+export const TextHeadingH3: StoryObj<TypographyProps> = {
+  args: {
+    children: 'Text',
+    element: 'h3',
+    size: 'title3',
+    className: 'font-extrabold'
+  }
+}
+
+export const TextParagraph: StoryObj<TypographyProps> = {
+  args: {
+    children: 'Text',
+    element: 'p',
+    size: 'md',
+    className: 'text-primary'
+  }
+}
